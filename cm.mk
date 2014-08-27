52570950a30350d9fedcc155a710d2c4b824d91f
fefifofum <jorsellis@gmail.com>
2014-08-27 21:00:39 +0200

    Initial commit

---
+++ cm.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/xiaomi/armani/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := armani
+PRODUCT_NAME := cm_armani
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := HM 1S
---
+++ device.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH := device/xiaomi/armani
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/kernel:kernel \
+    $(LOCAL_PATH)/dt.img:dt.img \
+    $(LOCAL_PATH)/fstab.qcom:root/fstab.qcom
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_NAME := armani
