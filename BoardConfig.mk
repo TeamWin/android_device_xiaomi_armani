d7e2b54952b6470c4adf5ede53c4da6e35ac87e9
fefifofum <jorsellis@gmail.com>
2014-09-11 02:51:34 +0200

    Revert "Use correct CPU_VARIANT"
    This reverts commit f677f03944e773fa8dddd32ec41f10cbdc28befd.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,7 +12,7 @@ TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
-TARGET_CPU_VARIANT := cortex-a7
+TARGET_CPU_VARIANT := krait
 ARCH_ARM_HAVE_TLS_REGISTER := true
 
 # Flags
