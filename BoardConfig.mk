674f9a58a2f2e948066ba72f661e70bebda7830a
Kra1o5 <kra1o5x@gmail.com>
2014-12-28 21:29:40 +0100

    Fix UI

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,3 +45,4 @@ TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_DEFAULT_EXTERNAL_STORAGE := true
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
