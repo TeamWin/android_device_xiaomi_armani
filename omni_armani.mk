9589ad5eb9d2013b577b7379161487d5b896865b
fefifofum <jorsellis@gmail.com>
2015-02-03 20:24:25 +0100

    Adapt to omni

--- omni_armani.mk
+++ omni_armani.mk
@@ -3,7 +3,7 @@ $(call inherit-product, device/xiaomi/armani/device.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := armani
-PRODUCT_NAME := cm_armani
+PRODUCT_NAME := omni_armani
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := HM 1S
