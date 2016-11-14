f87c35647029b934b14a6d33c722324219129cea
fefifofum <jorsellis@gmail.com>
2016-11-14 22:40:57 +0100

    Add androidboot.bootdevice=msm_sdcc.1 to cmdline
    Change-Id: Iba1a1deea4df21255cb320aa47abd0ff9ef6f5ae

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,7 +14,7 @@ TARGET_NO_BOOTLOADER := true
 # Kernel
 TARGET_PREBUILT_KERNEL := device/xiaomi/armani/kernel
 TARGET_CUSTOM_KERNEL_HEADERS := device/xiaomi/armani/include
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=armani
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.bootdevice=msm_sdcc.1 androidboot.hardware=armani
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --dt device/xiaomi/armani/dt.img
 
